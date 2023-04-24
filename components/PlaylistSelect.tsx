2375d7c1be6afce78417d897b616f67e4e296718
mitaryuto <mitaryuto@gmail.com>
2023-04-25 01:54:28 +0900

    Fix: プレイリストのカード表示にページネーション付与

--- components/PlaylistSelect.tsx
+++ components/PlaylistSelect.tsx
@@ -7,48 +7,81 @@ interface PlaylistSelectProps {
     onPlaylistSelected: (playlistId: string) => void;
 }
 
-const PlaylistSelect: React.FC<PlaylistSelectProps> = ({
-                                                           accessToken,
-                                                           onPlaylistSelected,
-                                                       }) => {
-    const [playlists, setPlaylists] = useState<PlaylistObjectSimplified[]>([]);
+const PlaylistSelect: React.FC<PlaylistSelectProps> =
+    ({
+         accessToken,
+         onPlaylistSelected,
+     }) => {
+        const [playlists, setPlaylists] = useState<PlaylistObjectSimplified[]>([]);
+        const [currentPage, setCurrentPage] = useState(0);
+        const maxPage = Math.ceil(playlists.length / 5) - 1;
 
-    useEffect(() => {
-        const fetchPlaylists = async () => {
-            try {
-                const userPlaylists = await getUserPlaylists(accessToken);
-                setPlaylists(userPlaylists);
-            } catch (error) {
-                console.error("Error fetching playlists", error);
+        useEffect(() => {
+            const fetchPlaylists = async () => {
+                try {
+                    const userPlaylists = await getUserPlaylists(accessToken);
+                    setPlaylists(userPlaylists);
+                } catch (error) {
+                    console.error("Error fetching playlists", error);
+                }
+            };
+
+            if (accessToken) {
+                fetchPlaylists();
             }
+        }, [accessToken]);
+
+        const prevPage = () => {
+            setCurrentPage((prev) => (prev > 0 ? prev - 1 : prev));
         };
 
-        if (accessToken) {
-            fetchPlaylists();
-        }
-    }, [accessToken]);
+        const nextPage = () => {
+            setCurrentPage((prev) => (prev < maxPage ? prev + 1 : prev));
+        };
 
-    const handlePlaylistClick = (playlistId: string) => {
-        onPlaylistSelected(playlistId);
-    };
-
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {playlists.map((playlist) => (
-                <div
-                    key={playlist.id}
-                    className="bg-white rounded shadow p-4 cursor-pointer"
-                    onClick={() => handlePlaylistClick(playlist.id)}
-                >
-                    <img
-                        src={playlist.images[0]?.url || ""}
-                        alt={playlist.name}
-                        className="w-full h-48 object-cover mb-4 rounded"
-                    />
-                    <h2 className="text-xl font-bold">{playlist.name}</h2>
+        return (
+            <div>
+                <h2>Your Playlists</h2>
+                <div className="grid grid-cols-2 gap-4 items-start place-items-center">
+                    {playlists
+                        .slice(currentPage * 5, currentPage * 5 + 5)
+                        .map((playlist) => (
+                            <div
+                                key={playlist.id}
+                                className="w-64 p-4 border border-gray-200 rounded-lg shadow-sm cursor-pointer"
+                                onClick={() => onPlaylistSelected(playlist.id)}
+                            >
+                                <img
+                                    src={playlist.images[0]?.url || ""}
+                                    alt={playlist.name}
+                                    className="w-full h-32 object-cover rounded-lg"
+                                />
+                                <h3 className="mt-2 text-lg">{playlist.name}</h3>
+                            </div>
+                        ))}
                 </div>
-            ))}
-        </div>
-    );
-};
+                <div className="flex justify-center mt-4">
+                    <button
+                        onClick={prevPage}
+                        className={`bg-blue-500 text-white px-4 py-2 rounded-lg ${
+                            currentPage === 0 ? "opacity-50 cursor-not-allowed" : ""
+                        }`}
+                        disabled={currentPage === 0}
+                    >
+                        Previous
+                    </button>
+                    <p className="mx-4 my-auto">{currentPage + 1}</p>
+                    <button
+                        onClick={nextPage}
+                        className={`bg-blue-500 text-white px-4 py-2 rounded-lg ${
+                            currentPage === maxPage ? "opacity-50 cursor-not-allowed" : ""
+                        }`}
+                        disabled={currentPage === maxPage}
+                    >
+                        Next
+                    </button>
+                </div>
+            </div>
+        );
+    };
 export default PlaylistSelect;
